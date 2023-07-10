0ccc466b69c79456da3adbe691af3de178ba3bb6
Sean Warren <seanwarren77@gmail.com>
2023-07-10 10:28:31 -0400

    add jacks_house_class

--- spec/jacks_house_spec.rb
+++ spec/jacks_house_spec.rb
@@ -2,22 +2,21 @@ require "spec_helper"
 require_relative "../jacks_house"
 
 RSpec.describe JacksHouse do
+    let(:jacks_house) { JacksHouse.new } 
+
   describe "#recite" do
     it "recites the poem" do
-      jacks_house = JacksHouse.new
-      expect(jacks_house.recite).to eq "This is the house that Jack built.
-      This is the malt that lay in the house that Jack built.
-      This is the rat that ate the malt that lay in the house that Jack built.
-      This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-      "
+      poem = File.read("spec/fixtures/poem.txt")   
+      expect(jacks_house.recite).to eq poem
+    end 
+  end
+
+  xdescribe "#random_recite" do
+    it "recites the poem in random order" do
+        recite1 =  jacks_house.random_recite
+        recite2 = jacks_house.random_recite
+        expect(recite1.split("/n").count).to eq 12
+        expect(recite1 == recite2).to be false
     end
   end
 end
