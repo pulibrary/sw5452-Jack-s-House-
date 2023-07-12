88f4651cd2d04e048898597213040741f3d0127d
Sean Warren <seanwarren77@gmail.com>
2023-07-12 16:36:49 -0400

    added semi random to JacksHouse

--- spec/jacks_house_spec.rb
+++ spec/jacks_house_spec.rb
@@ -19,5 +19,23 @@ RSpec.describe JacksHouse do
         expect(recite1 == recite2).to be false
     end
   end
+
+  describe "#semi_recite" do
+    it "recites the poem in semi random order" do
+      recite1 =  jacks_house.semi_recite
+      recite2 = jacks_house.semi_recite
+      lines1 = recite1.split("\n")
+      lines2 = recite2.split("\n")
+      expect(lines1.count).to eq 12
+      expect(recite1 == recite2).to be false
+      lines1.each do |line|
+        expect(line).to include ("the house that Jack built.")
+      end
+      
+      lines2.each do |line|
+        expect(line).to include ("the house that Jack built.")
+      end
+    end
+  end
 end
 
